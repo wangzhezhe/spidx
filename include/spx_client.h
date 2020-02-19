07f64637841c284275184ab38198200a323872c4
wangzhezhe <w_hessen@126.com>
2020-02-19 11:11:05 +0800

    update spx client

--- include/spx_client.h
+++ include/spx_client.h
@@ -1,5 +1,5 @@
-#ifndef SPIDX_SERVER_H
-#define SPIDX_SERVER_H
+#ifndef SPIDX_CLIENT_H
+#define SPIDX_CLIENT_H
 
 #include <stdio.h>
 #include <assert.h>
@@ -8,21 +8,101 @@
 #include <abt.h>
 #include <margo.h>
 #include "types.h"
+#include "spx_common.h"
+#include "spx_error.h"
 
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+typedef struct spx_client* spx_client_t;
+#define SPX_CLIENT_NULL ((spx_client_t)NULL)
+typedef struct spx_provider_handle *spx_provider_handle_t;
+#define SPX_PROVIDER_HANDLE_NULL ((spx_provider_handle_t)NULL)
 
 //init the mochi service and expose the spidx RPC
-int spx_init(margo_instance_id mid);
+/**
+* @brief Creates a SPIDX client.
+*
+* @param[in] mid Margo instance
+* @param[out] client spidx client
+*
+* @return SPIDX_SUCCESS or error code defined in spidx_error.h
+*/
+int spx_client_init(margo_instance_id mid, spidx_client_t* client);
 
-//register the initial partition into current server
-int spx_register_hash_domain(uint32_t global_dim, uint64_t* global_lb, uint64_t* global_ub);
+/**
+ * @brief Finalizes a SPIDX client.
+ *
+ * @param[in] client SPIDX client to finalize
+ *
+ * @return SPIDX_SUCCESS or error code defined in spidx_error.h
+ */
+int spx_client_finalize(spx_client_t client);
 
-//update the spidx key
+
+/**
+ * @brief Creates a SPIDX provider handle.
+ *
+ * @param[in] client SPIDX client responsible for the provider handle
+ * @param[in] addr Mercury address of the provider
+ * @param[in] provider_id id of the provider
+ * @param[in] handle provider handle
+ *
+ * @return SPIDX_SUCCESS or error code defined in spidx_error.h
+ */
+int spx_provider_handle_create(
+        spx_client_t client,
+        hg_addr_t addr,
+        uint16_t provider_id,
+        spx_provider_handle_t* handle);
+
+/**
+ * @brief Increments the reference counter of a provider handle.
+ *
+ * @param handle provider handle
+ *
+ * @return ALPHA_SUCCESS or error code defined in spidx_error.h
+ */
+int spx_provider_handle_ref_incr(
+        spx_provider_handle_t handle);
+
+
+/**
+ * @brief Releases the provider handle. This will decrement the
+ * reference counter, and free the provider handle if the reference
+ * counter reaches 0.
+ *
+ * @param[in] handle provider handle to release.
+ *
+ * @return SPIDX_SUCCESS or error code defined in spidx_error.h
+ */
+int spx_provider_handle_release(spx_provider_handle_t handle);
+
+
+/**
+ * @brief Update the hash domain, this hash domain is labeld by the spx_index_key
+ *
+ * @param spx_index_key a generalized structure supporting multiple types
+ * @param spx_index_spatial the spatial information that containes the lower bound and the upper bound
+ * the spidx service will forward the update request to suitable service based on DHT and SFC
+ * 
+ * @return SPIDX_SUCCESS or error code defined in spidx_error.h
+ */
 int spx_update(spx_index_key_t spx_index_key, spx_index_spatial_t spx_index_spatial);
 
-//query the spidx key
+
+/**
+ * @brief query the id related with the hash domain
+ *
+ * @param[in] spx_index_spatial is spatial information that containes the lower bound and the upper bound
+ * the spidx service will forward the update request to suitable service based on DHT and SFC
+ * @param[out] list_of_spatial_id_bundle is a list, every element in this list is a boundle 
+ * that contains spatial information (bounding box) and an key, this key is setted by spx_update
+ * 
+ * @return SPIDX_SUCCESS or error code defined in spidx_error.h
+ */
 int spx_query(spx_index_spatial_t spx_index_spatial, spx_partition_id_bundle_t* list_of_spatial_id_bundle);
 
-//finalize the spidx service
-int spx_finalize();
 
 #endif
---
+++ include/spx_error.h
@@ -0,0 +1,11 @@
+
+
+#ifndef __SPIDX_ERROR_H
+#define __SPIDX_ERROR_H
+
+
+#define SPIDX_SUCCESS          0 /* Success */
+#define SPIDX_FAILURE         -1 /* Fail */
+
+
+#endif
